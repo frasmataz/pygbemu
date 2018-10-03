d88f3f4cb1f1098b42432075cdb482473bc7c49a
Fraser Sharp <fraserrsharp@gmail.com>
2018-10-03 10:55:30 +0100

    Refactor Graphics module into class

--- __main__.py
+++ __main__.py
@@ -1,13 +1,29 @@
-import graphics
+from graphics import Graphics
 import mmu
 import sys
+import sdl2
+import sdl2.ext
 
+global GB_PARAMS 
 GB_PARAMS = {
-    'screen_res': (160, 144)
+    'screen_res': (160, 144),
+    'clk_spd_mhz': '4.194'
 }
 
 def run():
-    gfx = graphics.init(GB_PARAMS)
+    gfx = Graphics(GB_PARAMS)
+
+    running = True
+    while running:
+      for e in sdl2.ext.get_events():
+        if e.type == sdl2.SDL_QUIT:
+          running = False
+          break
+        if e.type == sdl2.SDL_KEYDOWN:
+          if e.key.keysym.sym == sdl2.SDLK_ESCAPE:
+            running = False
+            break
+
 
 if __name__ == '__main__':
     sys.exit(run())
--- graphics.py
+++ graphics.py
@@ -1,31 +1,28 @@
-from __future__ import division
 import sdl2
 import sdl2.ext
 import sdl2.sdlgfx
 
-def draw_test_pattern(renderer, GB_PARAMS):
-    for x in range(0, GB_PARAMS['screen_res'][0]):
-        for y in range(0, GB_PARAMS['screen_res'][1]):
-            renderer.draw_point([x,y], sdl2.ext.Color((x/GB_PARAMS['screen_res'][0])*255, (y/GB_PARAMS['screen_res'][1]*255), 255))
+class Graphics:
+    def draw_test_pattern(self):
+        for x in range(0, self.GB_PARAMS['screen_res'][0]):
+            for y in range(0, self.GB_PARAMS['screen_res'][1]):
+                self.renderer.draw_point(
+                        [x,y], 
+                        sdl2.ext.Color(
+                            (x/self.GB_PARAMS['screen_res'][0])*255, 
+                            (y/self.GB_PARAMS['screen_res'][1])*255, 
+                            255
+                        )
+                    )
 
-    renderer.present()
+        self.renderer.present()
 
-def init(GB_PARAMS):
-    sdl2.ext.init()
-    window = sdl2.ext.Window("pygbemu", size=GB_PARAMS['screen_res'])
-    window.show()
-    
-    renderer = sdl2.ext.Renderer(window)
-    draw_test_pattern(renderer, GB_PARAMS)
-
-    running = True
-    while running:
-      for e in sdl2.ext.get_events():
-        if e.type == sdl2.SDL_QUIT:
-          running = False
-          break
-        if e.type == sdl2.SDL_KEYDOWN:
-          if e.key.keysym.sym == sdl2.SDLK_ESCAPE:
-            running = False
-            break
+    def __init__(self, GB_PARAMS):
+        self.GB_PARAMS = GB_PARAMS
+        sdl2.ext.init()
+        self.window = sdl2.ext.Window("pygbemu", size=GB_PARAMS['screen_res'])
+        self.window.show()
+       
+        self.renderer = sdl2.ext.Renderer(self.window)
+        self.draw_test_pattern()
 
