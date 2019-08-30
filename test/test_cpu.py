1df8874099ae8275ca98e4298535c535e4df7f1e
Fraser Sharp <fraserrsharp@gmail.com>
2019-08-30 16:00:44 +0100

    chore: Generalise reg tests, add first LD tests

--- test/test_cpu.py
+++ test/test_cpu.py
@@ -1,39 +1,48 @@
+import numpy as np
+from src.mmu import MMU
 from src.cpu import CPU
 
 def test_registers():
     cpu = CPU()
 
-    cpu.a = 0x55
-    cpu.f = 0xAA
-    assert cpu.get_af() == 0x55AA
+    regmap = {
+        'AF': ['A', 'F'],
+        'BC': ['B', 'C'],
+        'DE': ['D', 'E'],
+        'HL': ['H', 'L']
+    }
 
-    cpu.set_af(0xAA55)
-    assert cpu.a == 0xAA
-    assert cpu.f == 0x55
+    for _16reg, _8regs in regmap.items():
+        cpu.regs[_8regs[0]] = 0x55
+        cpu.regs[_8regs[1]] = 0xAA
+        assert cpu.get_reg_16(_16reg) == 0x55AA
 
-    cpu.b = 0x55
-    cpu.c = 0xAA
-    assert cpu.get_bc() == 0x55AA
-
-    cpu.set_bc(0xAA55)
-    assert cpu.b == 0xAA
-    assert cpu.c == 0x55
-
-    cpu.d = 0x55
-    cpu.e = 0xAA
-    assert cpu.get_de() == 0x55AA
-
-    cpu.set_de(0xAA55)
-    assert cpu.d == 0xAA
-    assert cpu.e == 0x55
-
-    cpu.h = 0x55
-    cpu.l = 0xAA
-    assert cpu.get_hl() == 0x55AA
-
-    cpu.set_hl(0xAA55)
-    assert cpu.h == 0xAA
-    assert cpu.l == 0x55
+        cpu.set_reg_16(_16reg, 0xAA55)
+        assert cpu.regs[_8regs[0]] == 0xAA
+        assert cpu.regs[_8regs[1]] == 0x55
 
     assert cpu.sp == 0x0000
     assert cpu.pc == 0x0000
+
+### Test opcodes
+
+# 8-bit loads
+
+def test_LD_nn_n():
+    # Map each register's opcode
+    ops = {
+        0x06: 'B',
+        0x0E: 'C',
+        0X16: 'D',
+        0X1E: 'E',
+        0X26: 'H',
+        0X2E: 'L'
+    }
+
+    for op, reg in ops.items():
+        rom_file = np.zeros(0x8000, dtype=np.uint8)
+        rom_file[0x0000] = op
+        rom_file[0x0001] = 0xAA
+        cpu = CPU(MMU(rom_file))
+        cpu.tick()
+        assert cpu.regs[reg] == 0xAA
